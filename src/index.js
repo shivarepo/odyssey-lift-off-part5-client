77aa9f8891a52104d367c72e704aa4c321fc40d4
Shivakumar Rajamanickam <raja15@sfofvfhx39sq05n.attlocal.net>
2023-01-18 21:44:56 -0500

    update to production uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  //uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-91e7.up.railway.app",
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
